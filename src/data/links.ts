172b60c8f1d0d2a7821b7c663064cd0f0f452908
Allenyou <i@allenyou.wang>
2024-07-25 23:37:36 +0800

    清除原有内容

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,87 +2,9 @@ import { LinkType } from "@/libs/types";
 
 export const links: LinkType[] = [
 	{
-		title: "MelloM的博客",
-		url: "https://blog.dmxvx.cc/",
-		image:
-			"https://blog-oss.allenyou.top/avatar/2764b0d411f9847464a866cb5500beed",
-		description: "写点无聊事",
-	},
-	{
-		title: "中二病晚期の物語",
-		url: "https://imfurry.com/",
-		image:
-			"https://blog-oss.allenyou.top/avatar/f28c0de50214c44f67d38e688495d6b7",
-		description: "一只狐狸敲出的一个奇迹",
-	},
-	{
-		title: "冰轩",
-		url: "https://www.bingxuan.org/",
-		image: "https://blog-oss.allenyou.top/image/64b399783643f.png",
-		description: "好的，这里是冰轩",
-	},
-	{
-		title: "狼林鱼池",
-		url: "https://blog.jiyu134.top/",
-		image: "https://blog-oss.allenyou.top/image/65c6353e8b4f9.jpg",
-		description: "风带来了故事的种子，时间使之发芽",
-	},
-	{
-		title: "圆周率的博客",
-		url: "https://yuanzj.top/",
-		image: "https://blog-oss.allenyou.top/image/64b399785795e.png",
-		description: "My Blog!",
-	},
-	// {
-	// 	title: "布丁の小窝",
-	// 	url: "https://furryowo.top/",
-	// 	image:
-	// 		"https://blog-oss.allenyou.top/avatar/7da7e82f04de94c795db0301c3397bb6",
-	// 	description: "一个有梦想的咸鱼",
-	// },
-	{
-		title: "挖站否",
-		url: "https://wzfou.com/",
-		image:
-			"https://blog-oss.allenyou.top/avatar/b87be896df439135e2972012a886b430",
-		description: "挖掘建站的乐趣",
-	},
-	{
-		title: "搅局’s blog",
-		url: "https://www.chinesehacker.org",
-		image: "https://blog-oss.allenyou.top/image/65594fd0af4ad.jpg",
-		description: "我寄愁心与明月，随君直到夜郎西",
-	},
-	// {
-	// 	title: "无名博客",
-	// 	url: "https://wuminboke.site/",
-	// 	image: "https://blog-oss.allenyou.top/image/65c636851bfd6.webp",
-	// 	description: "一个无聊的人罢了~",
-	// },
-	{
-		title: "吐槽大王部落格",
-		url: "https://www.tcdw.net/",
-		image:
-			"https://blog-oss.allenyou.top/avatar/70ae2023afad30dae905344325cece8f",
-		description: "正在学习接受不完美的世界！",
-	},
-	{
-		title: "孤熵小狼",
-		url: "https://blog.nixieka.top",
-		image: "https://blog-oss.allenyou.top/image/65ed6956adbf1.webp",
-		description: "趁年轻，多闯荡",
-	},
-	{
-		title: "阿方的博客",
-		url: "https://fang.blog.miri.site/",
-		image:
-			"https://blog-oss.allenyou.top/avatar/450ae8d0f847ca902ea88ee8ea42ea34",
-		description: "现实的引力太过沉重，任何飞扬的思想都会砰然坠地。",
-	},
-	{
-		title: "Cznorth的博客",
-		url: "https://www.cznorth.cn/",
-		image: "https://blog-oss.allenyou.top/image/660a838323b51.jpg",
-		description: "一个吃OI老本的ACMer",
+		title: "",
+		url: "",
+		image: "",
+		description: "",
 	},
 ];
