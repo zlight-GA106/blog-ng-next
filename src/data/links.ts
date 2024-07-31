85b9927bc05cf3d28f99fdb920680ccdc6468e4e
zlight-GA106 <2931632176@qq.com>
2024-07-31 19:19:40 +0800

    message

--- src/data/links.ts
+++ src/data/links.ts
@@ -3,7 +3,7 @@ import { LinkType } from "@/libs/types";
 export const links: LinkType[] = [
 	{
 		title: "秋实-Allenyou的小窝",
-		url: "allenyou.wang",
+		url: "https://www.allenyou.wang",
 		image: "https://blog-oss.allenyou.top/avatar/59b2fbdd507a84fe181b0af3e4ad7be3",
 		description: "稻花香里说丰年，听取WA声一片",
 	},
